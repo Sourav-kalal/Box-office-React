fe17001aaea9e36cb5704edd7ff0d939126e3189
769850 <souravkalal99@gmail.com>
2022-04-05 15:47:11 +0530

    fetched data in show.js

--- src/pages/show.js
+++ src/pages/show.js
@@ -5,13 +5,37 @@ import { apiGet } from '../misc/config';
 const Show = () => {
     const {id} = useParams();
     const [show, setshow] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
 
     useEffect(() => {
+
+        let isMounted = true;
+
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(res => {
-            setshow(res);
+            if (isMounted) {
+                setshow(res);
+                setIsLoading(false);
+            }
+        }).catch(err => {
+            if (isMounted) {
+                setIsLoading(false);
+                setError(err.message)
+            }
         })
+
+        return () => {
+            isMounted = false;
+        }
     }, [id]);
 
+    if (isLoading) {
+        return <div>Data is loaded</div>
+    }
+    if (error) {
+        return <div>Ther is an error {error}</div>
+    }
     console.log(show)
 
     return (
